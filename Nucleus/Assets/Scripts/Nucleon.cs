ef3931d083178a33796d87e16fb44ccbee0a33ea
Nico Hämäläinen <nico@sizeof.io>
2016-09-08 19:41:42 +0300

    Added Nucleus project

---
+++ Nucleus/Assets/Scripts/Nucleon.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Rigidbody))]
+public class Nucleon : MonoBehaviour {
+  public float attractionForce;
+  Rigidbody body;
+
+  void Awake() {
+    body = GetComponent<Rigidbody>();
+  }
+
+  void FixedUpdate() {
+    body.AddForce(transform.localPosition * -attractionForce);
+  }
+}
---
+++ Nucleus/Assets/Scripts/NucleonSpawner.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NucleonSpawner : MonoBehaviour {
+  public float timeBetweenSpawns;
+  public float spawnDistance;
+  public Nucleon[] nucleonPrefabs;
+
+  float timeSinceLastSpawn;
+
+  void FixedUpdate() {
+      Nucleon prefab = nucleonPrefabs[Random.Range(0, nucleonPrefabs.Length)];
+      Nucleon spawn = Instantiate<Nucleon>(prefab);
+      spawn.transform.localPosition = Random.onUnitSphere * spawnDistance;
+  }
+}
