60dc8bd444c99a45882584335421ebad3f5f85b1
Nico Hämäläinen <nico@sizeof.io>
2016-09-08 17:52:25 +0300

    Better fractal generation

--- Fractal/Assets/Scripts/Fractal.cs
+++ Fractal/Assets/Scripts/Fractal.cs
@@ -10,24 +10,56 @@ public class Fractal : MonoBehaviour {
   public int maximumDepth;
   private int currentDepth;
 
+  // The child fractal scale factor
+  public float childScale;
+
+  // Directions to extend to
+  private static Vector3[] childDirections = {
+    Vector3.up,
+    Vector3.right,
+    Vector3.left,
+    Vector3.forward,
+    Vector3.back
+  };
+
+  // Child extension orientations
+  private static Quaternion[] childOrientations = {
+    Quaternion.identity,
+    Quaternion.Euler(0f, 0f, -90f),
+    Quaternion.Euler(0f, 0f, 90f),
+    Quaternion.Euler(90f, 0f, 0f),
+    Quaternion.Euler(-90f, 0f, 0f)
+  };
+
 	// Use this for initialization
 	void Start () {
 	  gameObject.AddComponent<MeshFilter>().mesh = mesh;
     gameObject.AddComponent<MeshRenderer>().material = material;
     
     if (currentDepth < maximumDepth) {
-      new GameObject("Child")
-        .AddComponent<Fractal>()
-        .Initialize(this);
+      StartCoroutine(CreateChildren());
     }
 	}
 
-  private void Initialize(Fractal parent) {
+  private IEnumerator CreateChildren() {
+    for (int i = 0; i < childDirections.Length; i++) {
+      yield return new WaitForSeconds(Random.Range(0.1f, 0.5f));
+      new GameObject("Child")
+        .AddComponent<Fractal>()
+        .Initialize(this, i);  
+    }
+  }
+
+  private void Initialize(Fractal parent, int childIndex) {
     mesh = parent.mesh;
     material = parent.material;
     maximumDepth = parent.maximumDepth;
     currentDepth = parent.currentDepth + 1;
+    childScale = parent.childScale;
     transform.parent = parent.transform;
+    transform.localScale = Vector3.one * childScale;
+    transform.localPosition = childDirections[childIndex] * (0.5f + 0.5f * childScale);
+    transform.localRotation = childOrientations[childIndex];
   }
 	
 	// Update is called once per frame
