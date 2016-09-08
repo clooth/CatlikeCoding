0159926db6e691adaf1a452d5478be09cc6da032
Nico Hämäläinen <nico@sizeof.io>
2016-09-09 01:34:31 +0300

    Initial version of splines and curves

---
+++ SplinesCurves/Assets/Editor/BezierCurveInspector.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(BezierCurve))]
+public class BezierCurveInspector : Editor {
+    private BezierCurve curve;
+
+    // Handle properties
+    private Transform handleTransform;
+    private Quaternion handleRotation;
+
+    // Steps to draw on the curve
+    private const int lineSteps = 20;
+
+    // Drawing
+    private void OnSceneGUI() {
+        curve = target as BezierCurve;
+        handleTransform = curve.transform;
+        handleRotation = Tools.pivotRotation == PivotRotation.Local
+            ? handleTransform.rotation
+            : Quaternion.identity;
+
+        // Draw point handles
+        Vector3 p0 = ShowPoint(0);
+        Vector3 p1 = ShowPoint(1);
+        Vector3 p2 = ShowPoint(2);
+        Vector3 p3 = ShowPoint(3);
+
+        // Draw points
+        Handles.color = Color.gray;
+        Handles.DrawLine(p0, p1);
+        Handles.DrawLine(p2, p3);
+
+        // Draw bezier curve
+        Handles.color = Color.white;
+        Vector3 lineStart = curve.GetPoint(0f);
+        for (int i = 1; i <= lineSteps; i++) {
+            Vector3 lineEnd = curve.GetPoint(i / (float)lineSteps);
+            Handles.DrawLine(lineStart, lineEnd);
+            lineStart = lineEnd;
+        }
+
+        Handles.color = Color.green;
+        Handles.DrawLine(lineStart, lineStart + curve.GetDirection(0f));
+        for (int i = 1; i <= lineSteps; i++) {
+            Vector3 lineEnd = curve.GetPoint(i / (float)lineSteps);
+            Handles.color = Color.white;
+            Handles.DrawLine(lineStart, lineEnd);
+            Handles.color = Color.green;
+            Handles.DrawLine(lineEnd, lineEnd + curve.GetDirection(i / (float)lineSteps));
+            lineStart = lineEnd;
+        }
+    }
+
+    private Vector3 ShowPoint(int index) {
+        Vector3 point = handleTransform.TransformPoint(curve.points[index]);
+
+        // Check for handle changes
+        EditorGUI.BeginChangeCheck();
+        point = Handles.DoPositionHandle(point, handleRotation);
+        if (EditorGUI.EndChangeCheck()) {
+            Undo.RecordObject(curve, "Move Point");
+            EditorUtility.SetDirty(curve);
+            curve.points[index] = handleTransform.InverseTransformPoint(point);
+        }
+
+        return point;
+    }
+}
---
+++ SplinesCurves/Assets/Editor/LineInspector.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(Line))]
+public class LineInspector : Editor {
+    private void OnSceneGUI() {
+        Line line = target as Line;
+        Transform handleTransform = line.transform;
+
+        // Handle rotation based on local
+        Quaternion handleRotation = Tools.pivotRotation == PivotRotation.Local
+            ? handleTransform.rotation
+            : Quaternion.identity;
+
+        // Transform line points
+        Vector3 p0 = handleTransform.TransformPoint(line.p0);
+        Vector3 p1 = handleTransform.TransformPoint(line.p1);
+
+        // Draw line and handles
+        Handles.color = Color.white;
+        Handles.DrawLine(p0, p1);
+        Handles.DoPositionHandle(p0, handleRotation);
+        Handles.DoPositionHandle(p1, handleRotation);
+
+        // Make handles actually work
+        EditorGUI.BeginChangeCheck();
+        p0 = Handles.DoPositionHandle(p0, handleRotation);
+        if (EditorGUI.EndChangeCheck()) {
+            // Makes undo work
+            Undo.RecordObject(line, "Move Point");
+            // Makes "unsaved changes" work
+            EditorUtility.SetDirty(line);
+            // Transform from handle to line local coordinates
+            line.p0 = handleTransform.InverseTransformPoint(p0);
+        }
+        EditorGUI.BeginChangeCheck();
+        p1 = Handles.DoPositionHandle(p1, handleRotation);
+        if (EditorGUI.EndChangeCheck()) {
+            Undo.RecordObject(line, "Move Point");
+            EditorUtility.SetDirty(line);
+            line.p1 = handleTransform.InverseTransformPoint(p1);
+            EditorUtility.SetDirty(line);
+        }
+
+    }
+}
