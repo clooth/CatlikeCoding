caa906f58d0c9c4b6fcef9f62eec27d7162fbfb1
Nico Hämäläinen <nico@sizeof.io>
2016-09-08 17:36:55 +0300

    Added fractal camera and basic script

---
+++ Fractal/Assets/Scripts/Fractal.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Fractal : MonoBehaviour {
+  // Mesh and material for the fractal
+  public Mesh mesh;
+  public Material material;
+
+  // Depth settings for the fractal
+  public int maximumDepth;
+  private int currentDepth;
+
+	// Use this for initialization
+	void Start () {
+	  gameObject.AddComponent<MeshFilter>().mesh = mesh;
+    gameObject.AddComponent<MeshRenderer>().material = material;
+    
+    if (currentDepth < maximumDepth) {
+      new GameObject("Child")
+        .AddComponent<Fractal>()
+        .Initialize(this);
+    }
+	}
+
+  private void Initialize(Fractal parent) {
+    mesh = parent.mesh;
+    material = parent.material;
+    maximumDepth = parent.maximumDepth;
+    currentDepth = parent.currentDepth + 1;
+    transform.parent = parent.transform;
+  }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
