6c33cfb681322c393fc7ae02b072414017ad60c2
Nico Hämäläinen <nico@sizeof.io>
2016-09-08 20:00:14 +0300

    Initial object pool spawner

---
+++ ObjectPools/Assets/Scripts/Stuff.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class Stuff : MonoBehaviour {
+  public Rigidbody Body { get; private set; }
+
+  void Awake() {
+    Body = GetComponent<Rigidbody>();
+  }
+}
---
+++ ObjectPools/Assets/Scripts/StuffSpawner.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StuffSpawner : MonoBehaviour {
+  public float timeBetweenSpawns;
+  public Stuff[] stuffPrefabs;
+  public float velocity;
+  float timeSinceLastSpawn;
+
+  void FixedUpdate () {
+    timeSinceLastSpawn += Time.deltaTime;
+    if (timeSinceLastSpawn >= timeBetweenSpawns) {
+      timeSinceLastSpawn -= timeBetweenSpawns;
+      SpawnStuff();
+    }
+  }
+
+  void SpawnStuff () {
+    Stuff prefab = stuffPrefabs[Random.Range(0, stuffPrefabs.Length)];
+    Stuff spawn = Instantiate<Stuff>(prefab);
+    spawn.transform.localPosition = transform.position;
+    spawn.Body.velocity = transform.up * velocity;
+  }
+}
